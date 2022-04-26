bfda434acc115143a863d46d3f713a1719f39aee
jkuzmina <ju.a.kuzmina@yandex.ru>
2022-04-26 10:45:09 +0400

    MVVM

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,7 +1,9 @@
 package ru.netology.nmedia
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
 import ru.netology.nmedia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,40 +11,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        var post = Post(
-            1,
-            getResources().getString(R.string.title),
-            getResources().getString(R.string.content),
-            getResources().getString(R.string.published),
-            false,
-            10349,
-            400
-        )
-        with(binding){
-            title.text = post.author
-            published.text = post.published
-            content.text = post.content
-            likesCount.text = post.getCountStr(post.likesCount)
-            sharesCount.text = post.getCountStr(post.sharesCount)
-            if(post.likedByMe){
-                likes?.setImageResource(R.drawable.ic_baseline_favorite_24)
-            }
-            likes?.setOnClickListener {
-                post.likedByMe = !post.likedByMe
-                if(post.likedByMe) post.likesCount++ else post.likesCount--
+
+        val viewModel: PostViewModel by viewModels()
+        viewModel.data.observe(this) { post ->
+            with(binding) {
+                title.text = post.author
+                published.text = post.published
+                content.text = post.content
                 likesCount.text = post.getCountStr(post.likesCount)
-                likes.setImageResource(
-                    if(post.likedByMe) R.drawable.ic_baseline_favorite_24 else R.drawable.ic_baseline_favorite_border_24
-                )
-            }
-            shares?.setOnClickListener {
-                post.sharesCount++
                 sharesCount.text = post.getCountStr(post.sharesCount)
                 likes.setImageResource(
                     if(post.likedByMe) R.drawable.ic_baseline_favorite_24 else R.drawable.ic_baseline_favorite_border_24
                 )
             }
-
+        }
+        binding.likes.setOnClickListener {
+            viewModel.like()
+        }
+        binding.shares.setOnClickListener {
+            viewModel.share()
         }
 
     }
